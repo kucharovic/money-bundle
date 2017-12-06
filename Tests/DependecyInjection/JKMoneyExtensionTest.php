a2201b02651d2e1a5b0c8b07a53cefad5f2687d3
Jakub Kucharovic <jakub@kucharovic.cz>
2017-12-06 15:04:13 +0100

    Make Twig and Symfony\Form dependencies as optional

--- Tests/DependecyInjection/JKMoneyExtensionTest.php
+++ Tests/DependecyInjection/JKMoneyExtensionTest.php
@@ -31,14 +31,29 @@ class JKMoneyExtensionTest extends TestCase
         $loader->load(array($config), $container);
     }
 
-    public function testLoadConfiguration()
+    public function testLoadFormConfiguration()
     {
+        if (false === interface_exists('Twig_ExtensionInterface')) {
+            $this->markTestSkipped('Package `twig/twig` is not available.');
+        }
         $container = new ContainerBuilder();
         $container->setParameter('kernel.default_locale', 'cs');
         $loader = new JKMoneyExtension();
         $config = [];
         $loader->load(array($config), $container);
         $this->assertTrue($container->hasDefinition('JK\MoneyBundle\Form\Type\MoneyType'));
-        $this->assertTrue($container->hasDefinition('JK\MoneyBundle\Twig\MoneyExtension'));
     }
+
+    public function testLoadTwigConfiguration()
+    {
+        if (false === interface_exists('Symfony\Component\Form\FormInterface')) {
+            $this->markTestSkipped('Package `symfony/form` is not available.');
+        }        
+        $container = new ContainerBuilder();
+        $container->setParameter('kernel.default_locale', 'cs');
+        $loader = new JKMoneyExtension();
+        $config = [];
+        $loader->load(array($config), $container);
+        $this->assertTrue($container->hasDefinition('JK\MoneyBundle\Twig\MoneyExtension'));
+    }    
 }
